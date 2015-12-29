0fe074d7a8effd4231482cf92f6abeb15e066bf7
Qile Wang <wangqile123@gmail.com>
2015-12-29 04:37:55 -0500

    Functioning backend accomplished\!

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,42 @@ var router = express.Router();
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+/********************* Param functions ******************************/
+
+// Function automatically called for anything with :post in it
+router.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+
+	query.exec(function(err, post){
+		if(err){ return next(err); }
+		if(!post){ return next(new Error('can\'t find post')); }
+
+		req.post = post;
+		return next();
+	});
+});
+
+// Function automatically called for anything with :comment in it
+router.param('comment', function(req, res, next, id){
+	var query = Comment.findById(id);
+
+	query.exec(function(err, comment){
+		if(err){ return next(err); }
+		if(!comment){ return next(new Error('Can\'t find comment')); }
+
+		req.comment = comment;
+		return next();
+	});
+});
+
+/*********************** Actual Routes **************************/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
@@ -30,3 +59,46 @@ router.post('/posts', function(req, res, next) {
     res.json(post);
   });
 });
+
+router.get('/posts/:post', function(req, res){
+	req.post.populate('comments', function(err, post){
+		if(err){ return next(err); }
+
+		res.json(post);
+	});
+});
+
+router.put('/posts/:post/upvote', function(req, res, next){
+	req.post.upvote(function(err, post){
+		if(err){ return next(err); }
+
+		res.json(post);
+	});
+});
+
+router.post('/posts/:post/comments', function(req, res, next){
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save(function(err, comment){
+		if(err){ return next(err); }
+
+		req.post.comments.push(comment);
+		req.post.save(function(err, post){
+			if(err){ return next(err); }
+
+			res.json(comment);
+		});
+	});
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+	req.comment.upvote(function(err, post){
+		if(err){ return next(err); }
+
+		res.json(comment);
+	});
+});
+
+
+module.exports = router;
